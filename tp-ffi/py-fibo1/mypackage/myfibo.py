c9e8c2b1a287b9bc4c19e453661e61737dfc3713
devisme.valentin <valentin.devisme@etu.univ-littoral.fr>
2024-02-21 15:51:40 +0100

    fibo1

--- tp-ffi/py-fibo1/mypackage/myfibo.py
+++ tp-ffi/py-fibo1/mypackage/myfibo.py
@@ -4,4 +4,18 @@ def fibo_naive(n: int):
     return n if n < 2 else fibo_naive(n-1) + fibo_naive(n-2)
 
 # TODO implement fibo_iterative
+def fibo_iterative(n: int):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        n0 = 0
+        n1 = 1
+        for i in range(n-1):
+            tmp = n1
+            n1 = n0 + n1
+            n0 = tmp
+        return n1
+    
 
