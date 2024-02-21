48bd35b0b0a05f1b653f18c4899a25c3bdae389e
devisme.valentin <valentin.devisme@etu.univ-littoral.fr>
2024-02-21 16:24:56 +0100

    tp ffi - py-fibo2

--- tp-ffi/py-fibo2/cpp/myfibo.cpp
+++ tp-ffi/py-fibo2/cpp/myfibo.cpp
@@ -5,16 +5,40 @@ int fiboNaive(int n) {
     return n < 2 ? n : fiboNaive(n-1) + fiboNaive(n-2);
 }
 
-// TODO implement fiboIterative
+// implement fiboIterative
+
+int fiboIterative(int n) {
+    if (n == 0) return 0;
+    else{
+        if (n == 1) return 1;
+        else {
+            int n0 = 0;
+            int n1 = 1;
+            for(int i = 1; i < n;i++){
+                int tmp = n1;
+                n1 = n0 + n1;
+                n0 = tmp;
+            }
+            return n1;
+        }
+    } 
+    
+}
+
+
 
 
 #include <pybind11/pybind11.h>
 
 PYBIND11_MODULE(myfibo, m) {
 
-    // TODO export fiboNaive (as fibo_naive)
+    // export fiboNaive (as fibo_naive)
 
-    // TODO export fiboIterative (as fibo_iterative)
+    m.def("fibo_naive", &fiboNaive);
+
+    // export fiboIterative (as fibo_iterative)
+
+    m.def("fibo_iterative", &fiboIterative);
 
 }
 
--- tp-ffi/py-fibo2/scripts/main1.py
+++ tp-ffi/py-fibo2/scripts/main1.py
@@ -1,9 +1,16 @@
+import myfibo as mf
 
 if __name__ == '__main__':
 
     print('TODO')
     
-    # TODO print fibo_naive(i) for i from 0 to 10
+    # print fibo_naive(i) for i from 0 to 10
+    for i in range(11):
+        print(f"fibo_naive({i}) = {mf.fibo_naive(i)}")
+    
+    print()
 
-    # TODO print fibo_iterative(i) for i from 0 to 10
+    # print fibo_iterative(i) for i from 0 to 10
+    for i in range(11):
+        print(f"fibo_iterative({i}) = {mf.fibo_iterative(i)}")
 
