6f09ce1e0771cef11186b2ee3a0d1bc521c8f357
devisme.valentin <valentin.devisme@etu.univ-littoral.fr>
2024-02-22 15:34:47 +0100

    tp1 main.py modif

--- tp-ffi/py-fibo1/scripts/main.py
+++ tp-ffi/py-fibo1/scripts/main.py
@@ -1,16 +1,14 @@
 import mypackage.myfibo as mf
 
 if __name__ == '__main__':
-
-    print('TODO')
     
-    # TODO print fibo_naive(i) for i from 0 to 10
+    # print fibo_naive(i) for i from 0 to 10
     for i in range(11):
         print(f"fibo_naive({i}) = " + str(mf.fibo_naive(i)))
 
     print()
 
-    # TODO print fibo_iterative(i) for i from 0 to 10
+    # print fibo_iterative(i) for i from 0 to 10
     for i in range(11):
         print(f"fibo_iterative({i}) = " + str(mf.fibo_iterative(i)))
 
