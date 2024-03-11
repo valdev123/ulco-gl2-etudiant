441eae5b8f4387932245ce887cb4e83cfc2daac7
devisme.valentin <valentin.devisme@etu.univ-littoral.fr>
2024-03-11 14:48:30 +0100

    TP 3 : tp-refactoring/log-hs

--- tp-refactoring/log-hs/app/Main.hs
+++ tp-refactoring/log-hs/app/Main.hs
@@ -1,3 +1,5 @@
+import System.IO
+
 add3 :: Int -> Int 
 add3 = (+3)
 
@@ -12,9 +14,39 @@ mycompute v0 = do
     let v2 = mul2 v1
     return v2
 
+mycomputeFile :: Int -> IO Int
+mycomputeFile v0 = do
+    file <- openFile "file.txt" WriteMode  
+    hPutStrLn file ("add3 " ++ show v0)
+    let v1 = add3 v0
+    hPutStrLn file ("mul2 " ++ show v1)
+    let v2 = mul2 v1
+    hClose file
+    return v2
+
+mycomputeF :: (String -> IO ()) -> Int -> IO Int
+mycomputeF logF v0 = do
+    logF ("add3 " ++ show v0)
+    let v1 = add3 v0
+    logF ("mul2 " ++ show v1)
+    let v2 = mul2 v1
+    return v2
+    
+
 main :: IO ()
 main = do
     putStrLn "this is log-hs"
     
     res <- mycompute 18
     print res
+
+    resFile <- mycomputeFile 18
+    print resFile
+
+    resF <- mycomputeF putStrLn 18
+    print resF
+
+    hF <- openFile "log.txt" WriteMode
+    resFileF <- mycomputeF (hPutStrLn hF) 18
+    hClose hF
+    print resFileF
